3b2fd92ee121e02298d5a6ca1be822fe0ea44d17
absiddik676 <absiddik8200@gmail.com>
2023-08-05 23:30:00 +0600

    all book data get and new book and user data add api done

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+// Allow specific HTTP methods (e.g., GET, POST, OPTIONS, etc.).
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+// Allow specific HTTP headers in the request.
+header("Access-Control-Allow-Headers: Content-Type");
+include 'DbConnect.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+var_dump($conn);
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if($_GET['url'] === '/allbook'){
+        try {
+            $table_name = "allbook"; // Replace with the desired table name
+    
+            $sql = "SELECT * FROM $table_name";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+            // Set the response content type to JSON
+            header('Content-Type: application/json');
+    
+            // Store the JSON data in a variable
+            $json_data = json_encode($data);
+    
+            // Disable output buffering
+            while (ob_get_level()) {
+                ob_end_clean();
+            }
+    
+            // Output the JSON data
+            echo $json_data;
+        } catch (PDOException $e) {
+            // Handle any potential database errors gracefully
+            http_response_code(500);
+            echo json_encode(array('message' => 'Error retrieving data from the database.'));
+        }
+    }
+}
+
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   
+    if($_GET['url'] === '/addBook'){
+         try {
+            $book = json_decode(file_get_contents('php://input'));
+            $sql = "INSERT INTO allbook(id, title, author, category, copies, description, img) VALUES(null, :title, :author, :category, :copies, :description, :img)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':title', $book->title);
+            $stmt->bindParam(':author', $book->author);
+            $stmt->bindParam(':category', $book->category);
+            $stmt->bindParam(':copies', $book->copies);
+            $stmt->bindParam(':description', $book->description);
+            $stmt->bindParam(':img', $book->img);
+        
+            if ($stmt->execute()) {
+                $response = ['status' => 1, 'message' => 'Record created successfully.'];
+            } else {
+                $response = ['status' => 0, 'message' => 'Failed to create record.'];
+            }
+    
+            // Set the response content type to JSON
+            header('Content-Type: application/json');
+            // Output the JSON response
+            echo json_encode($response);
+        } catch (PDOException $e) {
+            // Handle any potential database errors gracefully
+            http_response_code(500);
+            echo json_encode(array('message' => 'Error creating record in the database.'));
+        }
+    }
+    elseif($_GET['url'] === '/saveUser'){
+        try {
+            $user = json_decode(file_get_contents('php://input'));
+            $sql = "INSERT INTO user(id, name, email, gender, role,  img) VALUES(null, :name, :email, :gender, :role, :img)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':name', $user->name);
+            $stmt->bindParam(':email', $user->email);
+            $stmt->bindParam(':gender', $user->gender);
+            $stmt->bindParam(':role', $user->role);
+            $stmt->bindParam(':img', $user->img);
+        
+            if ($stmt->execute()) {
+                $response = ['status' => 1, 'message' => 'Record created successfully.'];
+            } else {
+                $response = ['status' => 0, 'message' => 'Failed to create record.'];
+            }
+    
+            // Set the response content type to JSON
+            header('Content-Type: application/json');
+            // Output the JSON response
+            echo json_encode($response);
+        } catch (PDOException $e) {
+            // Handle any potential database errors gracefully
+            http_response_code(500);
+            echo json_encode(array('message' => 'Error creating record in the database.'));
+        }
+    }
+    
+}
+?>
